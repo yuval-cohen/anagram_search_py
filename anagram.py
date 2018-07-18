65e30f14817c9b4d4e3bb535372d8766400526c7
Yuval Cohen <yuval.email@gmail.com>
2018-07-18 22:31:30 +1000

    Updated docstrings in all py files Updated .gitignore

--- anagram.py
+++ anagram.py
@@ -1,27 +1,101 @@
+"""This module contains Anagram class that generates
+all possible anagrams of a word or phrase.
 
+Attributes:
+    none.
+"""
+
+import itertools
 
 from enum import Enum
 
 class NextAnagram(Enum):
+    """Enum class for the return value of Anagram.find_next_anagram."""
     NO_PHRASE = 1
     PHRASE_TOO_LONG = 2
     NEXT_FOUND = 3
     END_OF_OUTPUT = 4
 
 
-import itertools
-
 class Anagram:
+    """A class for generating all possible anagrams of a word or phrase.
+
+    E.g.:
+    phrase: cat
+    All anagrams generated:
+        act
+        ac t
+        a ct
+        a c t
+        atc
+        at c
+        a tc
+        a t c
+        cat
+        ca t
+        c at
+        c a t
+        cta
+        ct a
+        c ta
+        c t a
+        tac
+        ta c
+        t ac
+        t a c
+        tca
+        tc a
+        t ca
+        t c a
+
+    Usage:
+        anagram = Anagram("real fun")
+        while anagram.find_next_anagram() == NextAnagram.NEXT_FOUND:
+            next_anagram = anagram.get_next_anagram()
+            ...
+
+    Todo:
+        Change to yield next anagram:
+        anagram = Anagram("real fun")
+        for next_anagram in anagram.get_next_anagram():
+            ...
+
+    Attributes:
+        _phrase (str) - input phrase
+        _root_anagram (str) - single word anagram of phrase
+            e.g. if phrase = 'cat' all root anagrams are:
+            'act', 'atc', 'cat', 'cta', 'tac' and 'tca'
+        _multi_anagram (list) -  list of words as multi-word
+            anagram of specific root anagram.
+            E.g. root anagram = 'act'
+            multi anagrams are: 'a ct', 'ac t' 'a c t' and 'act'
+        _multi_anagram_index (int) - index of multi anagram of
+            a root anagram.
+        _max_phrase_len (int) - maximum phrase length.
+    """
 
     def __init__(self, _phrase = ""):
+        """Inits Anagram from an phrase str.
+
+        Args:
+            _phrase (str) - multi word phrase string
+        """
         self._phrase = _phrase
         self._root_anagram = ""
         self._multi_anagram = []
         self._multi_anagram_index = None
-        self._max_anagram_len = 10
+        self._max_phrase_len  = 10
 
 
-    def reset_phrase(self, _phrase):
+    def set_phrase(self, _phrase):
+        """Sets (new) phrase to Anagram object.
+
+        Args:
+            _phrase (str) - multi word phrase string
+
+        Returns:
+            none
+        """
         self._phrase = _phrase
         self._root_anagram = ""
         self._multi_anagram = []
@@ -29,6 +103,19 @@ class Anagram:
 
 
     def find_next_anagram(self):
+        """Finds the next anagram of the phrase.
+
+        Args:
+            none
+
+        Returns:
+            NextAnagram.NO_PHRASE - if no phrase
+            NextAnagram.PHRASE_TOO_LONG - phrase too long
+            NextAnagram.NEXT_FOUND - next anagram found
+                (call get_next_anagram to retrieve it)
+            NextAnagram.END_OF_OUTPUT - end of output
+                (all anagrams found)
+        """
         if self._phrase == "":
             return NextAnagram.NO_PHRASE
 
@@ -38,7 +125,7 @@ class Anagram:
             self._root_anagram = self._phrase.translate(punct_table)
             self._root_anagram = self._root_anagram.replace(" ", "")
             self._root_anagram = self._root_anagram.lower()
-            if len(self._root_anagram) > self._max_anagram_len:
+            if len(self._root_anagram) > self._max_phrase_len :
                 return NextAnagram.PHRASE_TOO_LONG
 
             # create initial root anagram */
@@ -60,12 +147,22 @@ class Anagram:
 
 
     def get_next_anagram(self):
+        """Gets next anagram of Anagram object
+        following a call of find_next_anagram
+        that returns NextAnagram.NEXT_FOUND
+
+        Args:
+            none
+
+        Returns:
+            _multi_anagram (list) -  multi-word anagram as list of (str) words.
+        """
         return self._multi_anagram
 
 
     def _next_multi_anagram(self):
         # input: _root_anagram, _multi_anagram_index
-        # output: _multi_anagram with all words
+        # output: _multi_anagram (list of words)
         self._multi_anagram = list()
 
         if self._multi_anagram_index == 0:
--- anagram_search.py
+++ anagram_search.py
@@ -1,8 +1,31 @@
+#!/usr/bin/python3
+"""A program searches all anagrams of a word or phrase that its
+words can be found in an input word-list UTF-8 text file.
+The program prints all those anagrams to the standard output.
+
+Example:
+    phrase: real fun
+    input file: .\word.list
+    python anagram_search.py word.list real fun
+
+Attributes:
+    none
+"""
+
 import sys
 from word_list import WordListSet as WordList, FindWord
 from anagram import Anagram, NextAnagram
 
 def create_word_list(file_name):
+    """Creates WordList object from  input word-list text file.
+
+    Args:
+        file_name (str) - full path input word-list UTF-8 text file
+            each word in single line.
+
+    Returns:
+         The created WordList object
+    """
     print("creating word-list from "+str(file_name)+ " file... ")
     word_list = WordList("word.list")
     print("[DONE]")
@@ -10,6 +33,15 @@ def create_word_list(file_name):
 
 
 def print_all_anagrams_in_word_list(phrase_str, word_list):
+    """Prints all anagrams of a phrase that its words found in word list.
+
+    Args:
+        phrase_str (str) - phrase
+        word_list (WordList) - WordList object already created
+
+    Returns:
+         none
+    """
     phrase = Anagram(phrase_str)
     next_anagram = list()
     print("anagrams found:")
@@ -25,6 +57,22 @@ def print_all_anagrams_in_word_list(phrase_str, word_list):
 
 
 def main(phrase_str, file_name):
+    """The main function of anagram_search program.
+
+    Args:
+        phrase_str (str) - phrase
+            e.g.:
+                'cat'
+                'I'm okay'
+                'funeral'
+                'real fun'
+                'you, me, her'
+        file_name (str) - full path input word-list UTF-8 text file
+            each word in single line.
+
+        Returns:
+            none
+    """
     word_list = create_word_list(file_name)
     print_all_anagrams_in_word_list(phrase_str, word_list)
 
